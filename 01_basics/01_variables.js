805124d8d8ab3f47ee2622c1e867ab2656709f75
Shivam Verma <108995086+ShivamVerma04@users.noreply.github.com>
2024-02-11 12:09:03 +0000

    datatypes learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "Shivam@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2//  Not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use "Var"
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
